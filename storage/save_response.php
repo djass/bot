f7a240ff6cd6703f7c6f1711733d0776e6f8e6ca
root <jasmin.martinique@gmail.com>
2014-06-09 21:03:30 +0200

    Updates search

--- storage/save_response.php
+++ storage/save_response.php
@@ -16,123 +16,159 @@
 		return $p;
 	}
 
+
 	function save_procedure_in_aiml_form($question,$procedure){
-		
-		$question_clean = $question['clean'];
-		$question_en_clair = $question['clair'];
-		// on se connecte à notre base
 		$base = mysql_connect ('localhost', 'root', 'toor');
 		mysql_select_db ('response', $base) ;
 		mysql_query("SET NAMES UTF8");
-		
-		$aiml = array();
+		$question_clean =$question['clean'];
+		$question_en_clair = $question['clair']; 
 
-		$template_still_not =  "";
-	    $template_init = "";
-	    $template_close = "";
-
-		foreach ($procedure as $key => $value) {
-
-			if(array_key_exists($key+1, $procedure)) $next_key = $key+1; else $next_key = -1;
-
-			if($key == 0){				
-				if(!is_null($value['title']))$template_init = 'Pour résoudre votre souci, vous devriez '.strtolower($value['title']).'.<br/><br/>Pour ce se faire la procedure a suivre est la suivante:<br/>';
-				$template_init .= nettoyage(addslashes($value['desc'])).'
-				<br/><br/>Dites moi ci cela résout votre probleme.<think><set name="'.urlencode($question_clean).'">'.$next_key.'</set></think>';
-			}
-			else{ 
-				$template_still_not .= '
-					<li name="'.urlencode($question_clean).'" value="'.$key.'">
-						Ok. J ai peut etre une autre solution pour vous :<br/>
-						'.nettoyage(addslashes($value['title'])).'<br/><br/>
-						'.nettoyage(addslashes($value['desc'])).'
-						Essayez cela, si vous avez encore des soucis dites moi.
-	                    <br/><br/>Dites moi ci cela résout votre probleme.<think><set name="'.urlencode($question_clean).'">'.$next_key.'</set></think>
-	                </li>
-	            ';
-
-	            if($next_key == -1)
-	            	$template_still_not .= '
-					<li name="'.urlencode($question_clean).'" value="'.$next_key.'">
-						Je suis à court d idée...contactez le fabricant de votre ordinateur ou l’assistance technique pour obtenir une assistance.
-            			<think><set name="topic">mode_attente</set></think>
-	                </li>
-	            ';
-			}
-		}
-
-		
-		if(!is_null($template_init)){ 
-			// lancement de la requete
+		if(empty($procedure)){
+			//echo "Sauvegarde error :".$question_en_clair;
 			$sql = "INSERT INTO `bot_hermione`.`aiml` (`id`, `bot_id`, `aiml`, `pattern`, `thatpattern`, `template`, `topic`, `filename`) 
-			VALUES (NULL, '1', '', '$question_en_clair', '', '$template_init', 'mode_resol_panne', 'from_internet');
-			";  
+				VALUES (NULL, '1', '', '$question_en_clair', '', 'Je n ai rien trouvé concernant ce problème, ni sur internet, ni dans mes connaissances, je vous conseille donc de consulter un humain ...', 'mode_resol_panne', 'from_internet');
+				";  
+			mysql_query ($sql) or die ('Erreur SQL !'.$sql.'<br />'.mysql_error());	mysql_close();
+			
+			$sql = "INSERT INTO `bot_hermione`.`aiml` (`id`, `bot_id`, `aiml`, `pattern`, `thatpattern`, `template`, `topic`, `filename`) 
+				VALUES (NULL, '1', '', '$question_en_clair', '', 'Je n ai rien trouvé concernant ce problème, ni sur internet, ni dans mes connaissances, je vous conseille donc de consulter un humain ...', '', 'from_internet');
+				";  
 			mysql_query ($sql) or die ('Erreur SQL !'.$sql.'<br />'.mysql_error());	mysql_close();
 		}
+		else
+		{ 
+			
+			
+			
+			$aiml = array();
+	
+			$template_still_not =  "";
+		    $template_init = "";
+		    $template_close = "";
+	
+			foreach ($procedure as $key => $value) {
+	
+				if(array_key_exists($key+1, $procedure)) $next_key = $key+1; else $next_key = -1;
+	
+				if($key == 0){				
+					if(!is_null($value['title']))$template_init = 'Pour résoudre votre souci, vous devriez '.strtolower($value['title']).'.<br/><br/>Pour ce se faire la procedure a suivre est la suivante:<br/>';
+					$template_init .= nettoyage(addslashes($value['desc'])).'
+					<br/><br/>Dites moi ci cela résout votre probleme.<think><set name="'.urlencode($question_clean).'">'.$next_key.'</set></think>';
+				}
+				else{ 
+					$template_still_not .= '
+						<li name="'.urlencode($question_clean).'" value="'.$key.'">
+							Ok. J ai peut etre une autre solution pour vous :<br/>
+							'.nettoyage(addslashes($value['title'])).'<br/><br/>
+							'.nettoyage(addslashes($value['desc'])).'
+							Essayez cela, si vous avez encore des soucis dites moi.
+		                    <br/><br/>Dites moi ci cela résout votre probleme.<think><set name="'.urlencode($question_clean).'">'.$next_key.'</set></think>
+		                </li>
+		            ';
+	
+		            if($next_key == -1)
+		            	$template_still_not .= '
+						<li name="'.urlencode($question_clean).'" value="'.$next_key.'">
+							Je suis à court d idée...contactez le fabricant de votre ordinateur ou l’assistance technique pour obtenir une assistance.
+	            			<think><set name="topic">mode_attente</set></think>
+		                </li>
+		            ';
+				}
+			}
+	
+			
+			if(!is_null($template_init)){ 
+				// en mode attente
+				$sql = "INSERT INTO `bot_hermione`.`aiml` (`id`, `bot_id`, `aiml`, `pattern`, `thatpattern`, `template`, `topic`, `filename`) 
+				VALUES (NULL, '1', '', '$question_en_clair', '', '$template_init', 'mode_resol_panne', 'from_internet');
+				";  
+				mysql_query ($sql) or die ('Erreur SQL !'.$sql.'<br />'.mysql_error());	mysql_close();
 
-		if(!is_null($template_init)){ 
-			$base = mysql_connect ('localhost', 'root', 'toor');
-			mysql_select_db ('bot_hermione', $base) ;
-			// lancement de la requete
-			$sql = "INSERT INTO `bot_hermione`.`aiml` (`id`, `bot_id`, `aiml`, `pattern`, `thatpattern`, `template`, `topic`, `filename`) 
-			VALUES (NULL, '1', '', 'toujours pas', '', '<condition>$template_still_not</condition>', 'mode_resol_panne', 'from_internet');
-			";  
-			mysql_query ($sql) or die ('Erreur SQL !'.$sql.'<br />'.mysql_error());	mysql_close();
+				// en mode resolution
+				$sql = "INSERT INTO `bot_hermione`.`aiml` (`id`, `bot_id`, `aiml`, `pattern`, `thatpattern`, `template`, `topic`, `filename`) 
+				VALUES (NULL, '1', '', '$question_en_clair', '', '$template_init', '', 'from_internet');
+				";  
+				mysql_query ($sql) or die ('Erreur SQL !'.$sql.'<br />'.mysql_error());	mysql_close();
+			}
+	
+			if(!is_null($template_init)){   
+				$sql = "INSERT INTO `bot_hermione`.`aiml` (`id`, `bot_id`, `aiml`, `pattern`, `thatpattern`, `template`, `topic`, `filename`) 
+				VALUES (NULL, '1', '', 'toujours pas', '', '<condition>$template_still_not</condition>', '', 'from_internet');
+				";  
+				mysql_query ($sql) or die ('Erreur SQL !'.$sql.'<br />'.mysql_error());	mysql_close();
+				
+
+				$sql = "INSERT INTO `bot_hermione`.`aiml` (`id`, `bot_id`, `aiml`, `pattern`, `thatpattern`, `template`, `topic`, `filename`) 
+				VALUES (NULL, '1', '', 'toujours pas', '', '<condition>$template_still_not</condition>', 'mode_resol_panne', 'from_internet');
+				";  
+				mysql_query ($sql) or die ('Erreur SQL !'.$sql.'<br />'.mysql_error());	mysql_close();
+			}
 		}
 	}
 
 	function get_procedure_from_microsoft($url){
 		$procedure = array();
-		$html = file_get_html($url);
-		$a = current($html->find('a.link_expandAll'));
-		 
-		if(is_object($a)){
-			foreach($html->find('h4.title_section') as $article) {  
-			    $item['title']     = $article->plaintext;
-			   	$item['desc']  = null;
-			    $procedure[] = $item;
-			} 
-			
-			$i = -1;
-			foreach($html->find('div.collapse') as $article) {  
-			    $i++;
-			    $procedure[$i]['desc'] = saut_apres_phrases($article->plaintext);
-			    
-			   
-			}  
-		}else{
-			// Find all article blocks
-			//echo count($html->find('div.procedure'));
-			foreach($html->find('div.procedure') as $article) {
-				if($article->find('ol.ordered_dec', 0)->plaintext != null){
-					$item['title']     = $article->find('h3.title_procedure', 0)->plaintext;
-				    $item['desc']     = saut_apres_phrases($article->find('ol.ordered_dec', 0)->plaintext);
+		
+		if( is_object(file_get_html($url)) ){
+
+			$html = file_get_html($url);
+
+			if(is_object( current($html->find('a.link_expandAll')) ) ){				
+				$a = current($html->find('a.link_expandAll'));
+				foreach($html->find('h4.title_section') as $article) {  
+				    $item['title']     = $article->plaintext;
+				   	$item['desc']  = null;
 				    $procedure[] = $item;
-				}  					
-				    
-				 
+				} 
+				
+				$i = -1;
+				foreach($html->find('div.collapse') as $article) {  
+				    $i++;
+				    $procedure[$i]['desc'] = saut_apres_phrases($article->plaintext);				   
+				}  
+			}else{
+				// Find all article blocks
+				//echo count($html->find('div.procedure'));
+				foreach($html->find('div.procedure') as $article) {
+					if($article->find('ol.ordered_dec', 0)->plaintext != null){
+						$item['title']     = $article->find('h3.title_procedure', 0)->plaintext;
+					    $item['desc']     = saut_apres_phrases($article->find('ol.ordered_dec', 0)->plaintext);
+					    $procedure[] = $item;
+					}  					
+					    
+					 
+				}
 			}
 		}
 
 		return $procedure;
 
 	}  
-	$useless_words = array("ai-","est-","a-","j-ai-","j-","le-","la-","un-",-"une-","mon-","ma-",
-		"probleme-","souci-","avec-","ne-","d-");
+	$useless_words = array("peux-","tu-","ai-","est-","a-","jai-","j-","le-","la-","un-","une-","mon-","ma-","mes-",
+		"probleme-","souci-","avec-","ne-","d-","de-","des-","les-","avec-","m'aider","d'aide");
 
 	$var = explode('|', $_GET['v']);
 	$url = $var[1];
 	$question_en_clair = str_replace("-"," ", $var[0]);
 	$question_clean = str_replace($useless_words, array(""), $var[0]);
 	$question = array("clair"=>$question_en_clair,"clean"=>$question_clean);
-	$procedure = get_procedure_from_microsoft($url);
 	//echo "<pre>";var_dump($procedure);echo "</pre>";
 	//echo "(".$question['clair'].") ";
-	if(empty($procedure)){
-			echo "Je n'ai rien trouvé. Pouvez vous reformuler votre probleme s'il vous plaît?";}
-	else{
-			echo "J'ai terminé mes petites recherches :)";
-			save_procedure_in_aiml_form($question,$procedure);
+	if(filter_var($url, FILTER_VALIDATE_URL)){
+		$procedure = get_procedure_from_microsoft($url);
+		 
+		if(empty($procedure)){
+				echo "Je n'ai rien trouvé. Pouvez vous reformuler votre probleme s'il vous plaît? <br/>
+				Peut etre trouverez vous une solution sur cette <a target='_blank' href=".$url.">page</a>.";}
+		else{
+				echo "J'ai peut etre trouvé quelque chose pour vous. :)";			
+		}
+		save_procedure_in_aiml_form($question,$procedure);
 	}
+	else{
+		echo "Oups! Je n arrive pas à acceder à internet ...";
+	}
+
 	
 ?>
