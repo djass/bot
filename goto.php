67c7cb6db586e157da4699d69d20be7fc1a0e421
root <jasmin.martinique@gmail.com>
2014-06-10 21:31:20 +0200

    Cleaning

--- goto.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$url = 'https://somedomain.com/somesite/';
-$content = file_get_contents($_GET['page']);
-$first_step = explode( '<div class="topic_body">' , $content );
-$second_step = explode("</div>" , $first_step[1] );
-
-echo $second_step[0];
-?>
